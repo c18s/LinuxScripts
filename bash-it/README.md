40a188b085b480f12625e30333439e90a7a36f5c
Chagridsada Boonthus <chagridsada.inbox+github@gmail.com>
2018-02-21 15:49:31 +0700

    Add bash-it

---
+++ bash-it/README.md
@@ -0,0 +1,26 @@
+# Bash-it / Bash framework
+
+![Bash-it](logo.png "Bash-it")
+
+----
+
+[Bash-it][1] is a collection of community Bash commands and scripts for Bash 3.2+. 
+> Includes autocompletion, themes, aliases, custom functions, a few stolen pieces from Steve Losh, and more.
+
+----
+
+## Usage
+
+### Run script
+
+Default
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/c18s/LinuxScripts/master/bash-it/bash-it.sh | sh
+```
+
+## Reference
+
+- :octocat: <https://github.com/c18s/LinuxScripts/tree/master/bash-it>
+
+[1]: https://github.com/Bash-it/bash-it
---
+++ bash-it/bash-it.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if ! type git >/dev/null 2>&1; then
+  echo 'git: command not found'
+  exit 1
+fi
+
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh --silent
+
+if [ "$(id -u)" -ne 0 ]; then
+  sed -i "s/^.*export BASH_IT_THEME=.*$/export BASH_IT_THEME='axin'/" ~/.bash_profile >/dev/null 2>&1
+  sed -i "s/^.*export BASH_IT_THEME=.*$/export BASH_IT_THEME='axin'/" ~/.bashrc >/dev/null 2>&1
+else
+  sed -i "s/^.*export BASH_IT_THEME=.*$/export BASH_IT_THEME='binaryanomaly'/" ~/.bash_profile >/dev/null 2>&1
+  sed -i "s/^.*export BASH_IT_THEME=.*$/export BASH_IT_THEME='binaryanomaly'/" ~/.bashrc >/dev/null 2>&1
+fi
+
+## EOF
