7921f3f20c28abff1164139f5d14acb341f0377e
Chagridsada Boonthus <chagridsada.inbox+github@gmail.com>
2018-06-20 22:50:13 +0700

    Update bash-it install script

--- bash-it/bash-it.sh
+++ bash-it/bash-it.sh
@@ -5,17 +5,26 @@ if ! type git >/dev/null 2>&1; then
   exit 1
 fi
 
-## --- check user ---
 INSTALL_PATH=/root
 THEME=binaryanomaly
-if [ "$(id -u)" -ne 0 ]; then
+
+if [ "$(id -u)" -eq 0 ]; then
+  echo 'Bash-it: Rus script as root'
+  if [ ! -z "$USER" ] && [ -d "/home/$USER" ]; then
+    INSTALL_PATH=/home/$USER
+    THEME=dulcie
+    su -c "git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it" $USER
+    su -c "$INSTALL_PATH/.bash_it/install.sh --silent" $USER
+  else
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it
+    $INSTALL_PATH/.bash_it/install.sh --silent
+  fi
+else
   INSTALL_PATH=~
   THEME=dulcie
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it
+  $INSTALL_PATH/.bash_it/install.sh --silent
 fi
-
-cd $INSTALL_PATH
-git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it
-$INSTALL_PATH/.bash_it/install.sh --silent
 
 sed -i "s/^.*export BASH_IT_THEME=.*$/export BASH_IT_THEME='$THEME'/" $INSTALL_PATH/.bash_profile >/dev/null 2>&1
 sed -i "s/^.*export BASH_IT_THEME=.*$/export BASH_IT_THEME='$THEME'/" $INSTALL_PATH/.bashrc >/dev/null 2>&1
