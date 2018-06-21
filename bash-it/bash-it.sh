732e980c1c353ee36093cf7b821be608ff419ce4
Chagridsada Boonthus <chagridsada.inbox+github@gmail.com>
2018-06-21 10:53:37 +0700

    Fix HOME path before install script

--- bash-it/bash-it.sh
+++ bash-it/bash-it.sh
@@ -13,17 +13,17 @@ if [ "$(id -u)" -eq 0 ]; then
   if [ ! -z "$USER" ] && [ -d "/home/$USER" ]; then
     INSTALL_PATH=/home/$USER
     THEME=dulcie
-    su -c "cd $INSTALL_PATH && git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it" $USER
-    su -c "cd $INSTALL_PATH && $INSTALL_PATH/.bash_it/install.sh --silent" $USER
+    su -c "git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it" $USER
+    su -c "HOME=$INSTALL_PATH $INSTALL_PATH/.bash_it/install.sh --silent" $USER
   else
-    cd $INSTALL_PATH && git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it
-    cd $INSTALL_PATH && $INSTALL_PATH/.bash_it/install.sh --silent
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it
+    HOME=$INSTALL_PATH $INSTALL_PATH/.bash_it/install.sh --silent
   fi
 else
   INSTALL_PATH=~
   THEME=dulcie
-  cd $INSTALL_PATH && git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it
-  cd $INSTALL_PATH && $INSTALL_PATH/.bash_it/install.sh --silent
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git $INSTALL_PATH/.bash_it
+  HOME=$INSTALL_PATH $INSTALL_PATH/.bash_it/install.sh --silent
 fi
 
 sed -i "s/^.*export BASH_IT_THEME=.*$/export BASH_IT_THEME='$THEME'/" $INSTALL_PATH/.bash_profile >/dev/null 2>&1
